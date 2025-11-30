88ecf77ccdf830f1e8462db8b803ccef380e585b
Malik-Haziq <malikhaziq153@gmail.com>
2025-11-30 05:19:23 +0500

    fix build error

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import Desktop from "~/pages/Desktop";
 import Login from "~/pages/Login";
