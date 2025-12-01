cbdb5e5177caa20ea9f1f3dac871256eb41f9db1
Malik-Haziq <malikhaziq153@gmail.com>
2025-12-02 01:23:34 +0500

    update launch pad

--- src/configs/launchpad.ts
+++ src/configs/launchpad.ts
@@ -2,64 +2,46 @@ import type { LaunchpadData } from "~/types";
 
 const launchpadApps: LaunchpadData[] = [
   {
-    id: "flint",
-    title: "Flint",
-    img: "img/icons/launchpad/flint.png",
-    link: "https://github.com/Renovamen/flint"
+    id: "gmail",
+    title: "Gmail",
+    img: "img/icons/launchpad/gmail.png",
+    link: "mailto:malikhaziq153@gmail.com"
   },
   {
-    id: "oh-my-cv",
-    title: "Oh, My CV!",
-    img: "https://ohmycv.app/pwa-512x512.png",
-    link: "https://ohmycv.app"
+    id: "github",
+    title: "GitHub",
+    img: "img/icons/launchpad/github.png",
+    link: "https://github.com/Malik-Haziq"
   },
   {
-    id: "oh-vue-icons",
-    title: "Oh, Vue Icons!",
-    img: "img/icons/launchpad/oh-vue-icons.png",
-    link: "https://oh-vue-icons.js.org"
+    id: "linkedin",
+    title: "LinkedIn",
+    img: "img/icons/launchpad/linkedin.png",
+    link: "https://www.linkedin.com/in/malik-haziq-b5233a231/"
   },
   {
-    id: "gungnir",
-    title: "Gungnir",
-    img: "img/icons/launchpad/gungnir.png",
-    link: "https://vuepress-theme-gungnir.vercel.app/"
-  },
-  // {
-  //   id: "metallic",
-  //   title: "Metallic",
-  //   img: "img/icons/launchpad/meta.png",
-  //   link: "https://github.com/Renovamen/metallic"
-  // },
-  {
-    id: "what-if",
-    title: "My Notebook",
-    img: "img/icons/launchpad/notebook.png",
-    link: "https://note.zxh.io"
+    id: "twitter",
+    title: "Twitter",
+    img: "img/icons/launchpad/twitter.png",
+    link: "https://x.com/malikhaziq19"
   },
   {
-    id: "cube",
-    title: "Cube Solver",
-    img: "img/icons/launchpad/cube.png",
-    link: "https://cube.zxh.io/"
+    id: "instagram",
+    title: "Instagram",
+    img: "img/icons/launchpad/instagram.png",
+    link: "https://instagram.com/malikhaziq153"
   },
   {
-    id: "resume",
-    title: "Résumé",
-    img: "img/icons/launchpad/resume.png",
-    link: "https://resume.zxh.io/"
+    id: "facebook",
+    title: "Facebook",
+    img: "img/icons/launchpad/facebook.png",
+    link: "https://facebook.com/malikhaziq153"
   },
   {
-    id: "zelda",
-    title: "Webpage for Zelda",
-    img: "img/icons/launchpad/zelda.png",
-    link: "https://zelda.zxh.io/"
-  },
-  {
-    id: "fishmail",
-    title: "Fishmail",
-    img: "img/icons/launchpad/fishmail.png",
-    link: "https://fishmail.vercel.app/"
+    id: "devto",
+    title: "Dev.to",
+    img: "img/icons/launchpad/dev.png",
+    link: "https://dev.to/malikhaziq"
   }
 ];
 
